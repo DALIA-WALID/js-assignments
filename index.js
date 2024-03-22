c659f2a9eee3c142c5f2cc367f38ee2bb8769be9
DALIA-WALID <144299410+DALIA-WALID@users.noreply.github.com>
2024-03-23 00:35:36 +0200

    javascript

---
+++ index.js
@@ -0,0 +1,18 @@
+let a = 6;
+console.log(a)
+
+
+let para = document.createElement("div");
+para.innerText = "This is an assignment.";
+para.style.cssText = "color: blue;background-color:pink;font-size: 80px;font - weight: bold; text-align: center;font - family: Arial; ";
+document.getElementById("test").appendChild(para);
+console.table(["dalia","walid","elsaid","mosa"])
+console.log('%cHello, %cWorld!, %cdalia', 'color: blue;font-size: 40px;','color: green ;font-size: 40px;','color: red; font-size: 40px;');
+/*
+console.log("Iam In Console");
+let closeWin = document.write("Iam In Page");*/
+
+
+
+
+
